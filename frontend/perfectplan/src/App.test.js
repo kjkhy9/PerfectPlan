6dfdf4a36a8d89f03ba74c2ad6a13f9314a15b25
kjkhy9 <kjkhy9@umsystem.edu>
2025-12-15 17:53:56 -0600

    fixed frontend testing again

--- frontend/perfectplan/src/App.test.js
+++ frontend/perfectplan/src/App.test.js
@@ -1,10 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders login screen", () => {
+test("renders login screen after clicking login", () => {
   render(<App />);
 
-  expect(screen.getByText(/login/i)).toBeInTheDocument();
+  expect(screen.getByText(/welcome/i)).toBeInTheDocument();
+
+  fireEvent.click(screen.getByText(/login/i));
+
+
   expect(screen.getByPlaceholderText(/username/i)).toBeInTheDocument();
   expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument();
 });
